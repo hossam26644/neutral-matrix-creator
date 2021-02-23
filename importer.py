e69264d378d416ca06a75fa49d201345fc873d49
hossam26644 <hossam26644@live.com>
2021-02-23 13:07:58 +0100

    exports neutral matrix of coding sequences

--- importer.py
+++ importer.py
@@ -7,7 +7,7 @@ import sys
 class Importer(object):
 
     def __init__(self, filepath):
-        self.filepath = "./" + filepath + "/"
+        self.filepath = ""
 
     @staticmethod
     def get_lines_from_file(filename):
@@ -21,3 +21,24 @@ class Importer(object):
         with open(filename, 'r') as fp:
             payload = json.load(fp)
         return payload
+
+    @staticmethod
+    def export_json(filename, data):
+        with open(filename, 'w') as f:
+            json.dump(data, f)
+
+    @staticmethod
+    def export_dict_to_tsv(filename, data):
+        nuclutieds = ['A', 'C', 'G', 'T']
+        text = ''
+        for key, values in data.items():
+            text += key + '\t'
+            for nuclutied in nuclutieds:
+                text += str(values[nuclutied]) + '\t'
+            text += '\n'
+
+        with open(filename, 'w') as f:
+            f.write(text)
+
+
+
